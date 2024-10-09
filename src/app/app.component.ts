d0c528423883b333fe721d25dece4ded54296320
Mario Chita (EVIO) <mario.chita@go-evio.com>
2024-10-09 14:22:45 +0100

    My first app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CursoService } from './curso.service';
+import { CommonModule, NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud-Angular';
+  public photoList: any [] = [];
+
+
+  constructor(private _photoService : CursoService){
+
+ }
+
+ public  ngOnInit(): void {
+  this.getAllPhotos();
+ }
+
+
+  private getAllPhotos():void{
+   this._photoService
+     .getPhotos()
+     .subscribe((res:any)=>{
+        this.photoList = res;
+     });
+ }
+
+
 }
